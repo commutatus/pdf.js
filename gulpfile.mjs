b468e3aaaba33c9208e9ef87276f8ef0a0211a3e
Gurkirpal Singh <12171804+gpalsingh@users.noreply.github.com>
2024-08-21 20:10:05 +0530

    fix: Annotation drag

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2144,7 +2144,7 @@ gulp.task(
 );
 
 function packageJson() {
-  const VERSION = "5.0.34";
+  const VERSION = "5.0.35";
 
   const DIST_NAME = "@commutatus/pdfjs-dist";
   const DIST_DESCRIPTION = "Experimental build of Mozilla's PDF.js library.";
